aa9b3463280884fc9756dfee87057a7cc4651824
varshaprasad96 <varshaprasad96@gmail.com>
2021-09-09 12:15:09 -0700

    working

--- main.go
+++ main.go
@@ -34,9 +34,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	demov1 "github.com/example/memcached-operator/api/v1"
-	"github.com/example/memcached-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -50,7 +47,6 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(demov1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -91,13 +87,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controllers.NginxReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Nginx")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
